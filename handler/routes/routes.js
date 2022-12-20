80e714c181bca7a660e31a0026aa4967eaf66eb0
Deflandrenassim <nassim.def@gmail.com>
2022-12-20 19:32:33 +0100

    fix: call

--- handler/routes/routes.js
+++ handler/routes/routes.js
@@ -9,12 +9,12 @@ const routes = (router) => {
     router.get('/sneakers/', middleware.middlewareBase, Sneakers.readSneakers);
     router.post('/sneakers/', middleware.middlewareBase, Sneakers.createSneakers);
     router.get('/sneakers/:id', middleware.middlewareBase, Sneakers.readbyIdSneakers);
-    router.put('/sneakers/:id',  middleware.middlewareBase, Sneakers.readUpdateSneakers);
-    router.delete('/sneakers/:id',  middleware.middlewareBase, Sneakers.deleteSneakers);
-    router.post('/inscription/',  middleware.middlewareBase, Inscription.inscription);
-    router.post('/login/',  middleware.middlewareBase, Connexion.Authentification);
+    router.put('/sneakers/:id', middleware.middlewareBase, Sneakers.readUpdateSneakers);
+    router.delete('/sneakers/:id', middleware.middlewareBase, Sneakers.deleteSneakers);
+    router.post('/inscription/', middleware.middlewareBase, Inscription.inscription);
+    router.post('/login/', middleware.middlewareBase, Connexion.Authentification);
     router.post('/comment/:id', middleware.middlewareBase, Comment.PostComment);
-    router.get('/comment/:id',  middleware.middlewareBase, Comment.GetComments);
+    router.get('/comment/:id', middleware.middlewareBase, Comment.GetComment);
 
 };
 
